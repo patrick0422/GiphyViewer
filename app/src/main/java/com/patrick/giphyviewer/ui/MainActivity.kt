81f0889c64e6ccea799d3c998b3884e1f25b9521
양태웅 <didxodnd0422@gmail.com>
2022-04-09 20:04:08 +0900

    Apply PagingAdapter

--- app/src/main/java/com/patrick/giphyviewer/ui/MainActivity.kt
+++ app/src/main/java/com/patrick/giphyviewer/ui/MainActivity.kt
@@ -5,57 +5,38 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.patrick.giphyviewer.R
 import com.patrick.giphyviewer.data.model.GiphyResponse
 import com.patrick.giphyviewer.databinding.ActivityMainBinding
+import com.patrick.giphyviewer.util.DataComparator
 import com.patrick.giphyviewer.util.NetworkResult
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy {
-        DataBindingUtil.setContentView(
-            this,
-            R.layout.activity_main
-        )
-    }
+    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
     private val mainViewModel: MainViewModel by viewModels()
-    private val mAdapter: TrendingListAdapter by lazy { TrendingListAdapter() }
+    private val pagingAdapter: TrendingListAdapter by lazy { TrendingListAdapter(DataComparator) }
     private val staggeredGridLayoutManager: StaggeredGridLayoutManager by lazy {
         StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL).apply {
             gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) = with(binding) {
+    override fun onCreate(savedInstanceState: Bundle?): Unit = with(binding) {
         super.onCreate(savedInstanceState)
         lifecycleOwner = this@MainActivity
-        trendingList.adapter = mAdapter
+        trendingList.adapter = pagingAdapter
         trendingList.layoutManager = staggeredGridLayoutManager
 
-        getTrendingGifs()
-
-        mainViewModel.getTrendingGifsResponse.observe(this@MainActivity) {
-            handleTrendingGifsResponse(
-                it
-            )
-        }
-    }
-
-    private fun handleTrendingGifsResponse(response: NetworkResult<GiphyResponse>?) {
-        when (response) {
-            is NetworkResult.Success -> {
-                mAdapter.setData(response.data!!.data)
-            }
-            is NetworkResult.Error -> {
-                Toast.makeText(this, "Failed: ${response.message}", Toast.LENGTH_SHORT).show()
-            }
-            is NetworkResult.Loading -> {
-
+        lifecycleScope.launch {
+            mainViewModel.flow.collectLatest { pagingData ->
+                pagingAdapter.submitData(pagingData)
             }
         }
     }
-
-    private fun getTrendingGifs() = mainViewModel.getTrendingGifs()
 }
